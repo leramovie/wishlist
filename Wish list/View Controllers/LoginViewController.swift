a48b137f20e6f857ab95c0aaeb06a91cc77cb050
leramovie <leramovie@gmail.com>
2019-12-24 20:51:52 +0300

    Login and Registration forms are work

--- Wish list/View Controllers/LoginViewController.swift
+++ Wish list/View Controllers/LoginViewController.swift
@@ -7,12 +7,16 @@
 //
 
 import UIKit
+import Firebase
 
 class LoginViewController: UIViewController {
 
-    @IBOutlet weak var firstNameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    
     @IBOutlet weak var passwordTextField: UITextField!
+
     @IBOutlet weak var loginButton: UIButton!
+    
     @IBOutlet weak var errorLabel: UILabel!
     
     override func viewDidLoad() {
@@ -25,13 +29,37 @@ class LoginViewController: UIViewController {
         
         errorLabel.alpha = 0
         
-        Utilities.styleTextField(firstNameTextField)
+        Utilities.styleTextField(emailTextField)
         Utilities.styleTextField(passwordTextField)
         Utilities.styleFilledButton(loginButton)
     }
     
     @IBAction func loginTapped(_ sender: Any){
+        //Validate Text Fields
+        let email = emailTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        let password = passwordTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+       
+        //Signin in the user
+        Auth.auth().signIn(withEmail: email, password: password) {
+            (result, error) in
+            
+            if error != nil {
+                //Couldn't sign in
+                self.errorLabel.text = error!.localizedDescription
+                self.errorLabel.alpha = 1
+            }
+            else {
+                
+                let homeViewController = self.storyboard?.instantiateViewController(identifier: Constants.Storyboard.homeViewController) as? HomeViewController
+                       
+                    self.view.window?.rootViewController = homeViewController
+                    self.view.window?.makeKeyAndVisible()
+            }
+                
+            }
+        }
+        
         
     }
 
-}
+
